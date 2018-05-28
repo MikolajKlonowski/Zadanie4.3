70b41f8fee1afe8cde2633b2c8c81498a5419326
Mikołaj Klonowski <nick-93@o2.pl>
2018-05-28 17:51:49 +0200

    KOT

---
+++ src/Kot.java
@@ -0,0 +1,19 @@
+import java.sql.SQLOutput;
+
+public class Kot {
+    public static void main(String[] args) {
+
+ char znak1 = 75;
+ char znak2 = 79;
+ char znak3 = 84;
+
+
+        System.out.print(znak1);
+        System.out.print(znak2);
+        System.out.println(znak3);
+
+
+
+
+
+}}
